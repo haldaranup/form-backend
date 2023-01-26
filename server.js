1815dea9d202d593b4e128c6672039671d03a88b
Anup Haldar <haldaranup746@gmail.com>
2023-01-26 22:43:54 +0530

    add

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const userRoute = require("./routes/user.route.js");
+const home = require("./routes/home.route.js");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/myStore/api/user", userRoute);
+app.use("", home);
 
 
 app.listen(5000, async () => {
