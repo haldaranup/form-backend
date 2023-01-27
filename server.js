dd6adde0c14b14cfe47bcbd62de70c8b911c42bd
Anup Haldar <haldaranup746@gmail.com>
2023-01-27 13:49:59 +0530

    url

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 const express = require("express");
-const connectDB = require("./config/db");
 const userRoute = require("./routes/user.route.js");
 const home = require("./routes/home.route.js");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connected to MongoDB");
+});
+
 app.use("/myStore/api/user", userRoute);
-app.use("/", userRoute);
+app.use("/", home);
 
 
-app.listen(process.env.PORT || 3000, async () => {
-  try {
-    connectDB()
-  } catch (error) {
-    console.log(error);
-  }
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is up and running on port http://localhost:5000`);
 });
